7ef02dc8970636d930414b3ba989fc7f3d96fb2b
aea555 <akarahmet2002@gmail.com>
2022-08-15 21:12:00 +0300

    added functions to fetch blog datas.
    going to add functionality to allow editing & deleting posts.

--- src/Admin/Pages/ViewBlogs.js
+++ src/Admin/Pages/ViewBlogs.js
@@ -1,3 +1,4 @@
+import { v4 as uuid } from "uuid";
 import { useState, useEffect } from "react";
 import AdminHeader from "../partials/AdminHeader";
 import Blog from "../Tools/Others/Blog";
@@ -13,30 +14,35 @@ function ViewBlogs() {
     headers: {},
     data: data,
   };
-  let blogs;
+  const fetchData = async () => {
+    try {
+      const response = await axios(config);
+      setData(response.data.blogs);
+    } catch (err) {
+      console.log(`Request failed for reason: ${err}`);
+    }
+  };
   useEffect(() => {
-    const wait = async () => {
-      await fetchData();
-      blogs = datas;
-    };
-    wait();
+    fetchData();
   }, []);
-  const fetchData = () => {
-    axios(config)
-      .then((res) => {
-        setData(res.data.blogs);
-        return null;
-      })
-      .catch((error) => {
-        throw new Error(`request failed because of ${error}`);
-      });
+
+  const createDivs = () => {
+    const blogs = datas.map((data) => {
+      return (
+        <div key={uuid()} className="col-sm-6 mt-3">
+          <Blog key={data._id} title={data.title} content={data.content} _id={data._id} />
+        </div>
+      );
+    });
+    return blogs;
   };
 
-  console.log(blogs);
   return (
-    <div className="container-xl">
-      <AdminHeader />
-      <div className="row"></div>
+    <div key={uuid()} className="container-xl">
+      <AdminHeader key={uuid()} />
+      <div key={uuid()} className="row">
+        {datas.length ? createDivs() : null}
+      </div>
     </div>
   );
 }
--- src/Admin/Tools/Others/Blog.js
+++ src/Admin/Tools/Others/Blog.js
@@ -1,10 +1,13 @@
+import { Markup } from "interweave";
 function Blog(props) {
   return (
     <div className="card">
       <div className="card-body">
         <h5 className="card-title">{props.title}</h5>
         <h6 className="card-subtitle mb-2 text-muted">id: {props._id}</h6>
-        <p className="card-text">{props.content.slice(0, 60) + "..."}</p>
+        <p className="card-text">
+          <Markup content={props.content.slice(0, 60)} />
+        </p>
         <a href="#" className="card-link">
           Edit
         </a>
